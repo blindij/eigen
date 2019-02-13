6282a108b4491454698965e0efe38b94839837f3
Bjoern Lindi <blindij@users.noreply.github.com>
2019-02-13 18:43:57 +0100

    Shows example with scalar multiplication.

---
+++ tutorial/src/scalar_mul.cpp
@@ -0,0 +1,25 @@
+// File: scalar_mul.cpp
+//
+// Scalar multiplication and division
+//    matrix * scalar
+//    scalar * matrix
+//    matrix / scalar
+//
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+int main() 
+{
+   Matrix2d a;
+   a << 1, 2,
+     3, 4;
+   Vector3d v(1,2,3);
+   std::cout << "a * 2.5 =\n" << a * 2.5 << std::endl;
+   std::cout << "0.1 * v =\n" << 0.1 * a << std::endl;
+   std::cout << "Doing v *= 2;" << std::endl;
+   v *= 2;
+   std::cout << "Now v =\n" << v << std::endl;
+   return 0;
+}
